c2b78cfdf2ec9e1c589da193ca4e81a2d7e0c978
sima-zhu <sizhu@microsoft.com>
2021-11-10 12:53:04 -0800

    Sync eng/common directory with azure-sdk-tools repository for Tools PR 2245

--- eng/common/scripts/get-codeowners.ps1
+++ eng/common/scripts/get-codeowners.ps1
@@ -1,6 +1,5 @@
 param (
   $PathToOwners, # should be in relative form from root of repo. EG: sdk/servicebus
-  $RootDirectory, # ideally $(Build.SourcesDirectory)
   $ToolVersion = "", # Placeholder. Will update in next PR
   $ToolPath = "$env:AGENT_TOOLSDIRECTORY", # The place to check the tool existence. Put $(Agent.ToolsDirectory) as default
   $DevOpsFeed = "https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-net/nuget/v3/index.json", # DevOp tool feeds.
@@ -11,27 +10,41 @@ param (
 $EngOutputIndicator = ".*EngToolOutputProperty: (.*)"
 
 # Check if the retrieve-codeowners tool exsit or not.
-if (!(Test-Path "$ToolPath/retrieve-code-owners.exe")) {
-  Write-Host "Installing retrieve-codeowners tool first..."
-  dotnet tool install --tool-path $ToolPath --add-source $DevOpsFeed --version $ToolVersion "Azure.Sdk.Tools.RetrieveCodeOwners"
+function installRetrieveCodeOwnersTool() {
+  dotnet tool install --tool-path $ToolPath --add-source "$DevOpsFeed" --version "$ToolVersion" "Azure.Sdk.Tools.RetrieveCodeOwners"
 }
 
 # Get the json property from tool command.
-$codeOwnerConsoleOutput = & "$ToolPath/retrieve-code-owners" --target-directory "$PathToOwners" --root-directory "$WorkingDirectory" 
+function getCodeOwnersEntryFromCommand() {
+  return & "$ToolPath/retrieve-code-owners" --target-directory "$PathToOwners" --root-directory "$WorkingDirectory" 2>&1
+}
+function getCodeOwners() { 
+  $codeOwnerConsoleOutput = getCodeOwnersEntryFromCommand
 
-# Failed at the command of fetching code owners.
-if ($LASTEXITCODE -ne 0) {
-  Write-Host $codeOwnerConsoleOutput
-  return
+  # Failed at the command of fetching code owners.
+  if ($LASTEXITCODE -ne 0) {
+    Write-Host $codeOwnerConsoleOutput
+    return $null
+  }
+  # Parsing the json property from console output.
+  if (!($codeOwnerConsoleOutput -match $EngOutputIndicator)) {
+    Write-Host "There is something wrong with tool output. It does not include eng parsing format. $codeOwnerConsoleOutput"
+    return $null
+  }
+  $codeOwnersMatches = $Matches[1] 
+  $codeOwnerJson = $codeOwnersMatches | ConvertFrom-Json
+  if ($codeOwnersJson) {
+    Write-Host "There is something wrong with parsing logic. Check output: $codeOwnerConsoleOutput"
+    return $null
+  }
+  
+  $codeOwners = $codeOwnerJson.Owners -join ","
+  Write-Host "##vso[task.setvariable variable=$VsoVariable;]$codeOwners"
+  return $codeOwners
 }
 
-# Parsing the json property from console output.
-$codeOwnerConsoleOutput -match $EngOutputIndicator
-$codeOwnersMatches = $Matches[1] 
-$codeOwnerJson = $codeOwnersMatches | ConvertFrom-Json
-if (!$codeOwnersJson) {
-  Write-Host "There is something wrong with parsing logic. Check output: $codeOwnerConsoleOutput"
-  return
+if (!(Test-Path "$ToolPath/retrieve-code-owners.exe")) {
+  Write-Host "Installing retrieve-codeowners tool first..."
+  installRetrieveCodeOwnersTool
 }
-
-return $codeOwnerJson.Owners -join ","
+getCodeOwners
